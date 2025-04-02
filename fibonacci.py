da744f1892ed6a84a6d04604ba952fd5bd51a297
shuxian12 <rita910812@gmail.com>
2025-04-02 15:30:59 +0800

    Add bug

--- fibonacci.py
+++ fibonacci.py
@@ -6,7 +6,7 @@ def fibonacci(n : int):
         raise TypeError("Input must be an integer")
     
     if n == 0:
-        return 0
+        return 1
     elif n == 1:
         return 1
     return fibonacci(n - 1) + fibonacci(n - 2)
